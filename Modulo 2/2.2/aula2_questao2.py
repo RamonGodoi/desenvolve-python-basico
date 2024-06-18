13b890600de0286dadb2f9709eb7413e6ecfd3df
Ramon <127380359+RamonGodoi@users.noreply.github.com>
2024-06-18 17:27:38 -0300

    Add files via upload

---
+++ Modulo 2/2.2/aula2_questao2.py
@@ -0,0 +1,10 @@
+final="O resultado é"
+valor1=10
+valor2=3.5
+valor3=valor1+valor2
+print(type(final))
+print(type(valor1))
+print(type(valor2))
+print(type(valor3))
+print(final)
+print(valor3)
---
+++ Modulo 2/2.2/aula2_questao4.py
@@ -0,0 +1,7 @@
+juros=1.01
+saldo=500.0
+saldo=saldo*juros
+saldo=saldo*juros
+saldo=saldo*juros
+print("Apóes 3 meses meu novo saldo é")
+print(saldo)
