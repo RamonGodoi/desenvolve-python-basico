435d36f9f717d0129239b0e76636295776eacf9a
Ramon <127380359+RamonGodoi@users.noreply.github.com>
2024-06-21 05:38:38 -0300

    Delete 6.2 directory

--- 6.2/aula2_questao2.py
+++
@@ -1,8 +0,0 @@
-import random
-num_elementos=random.randint(5, 20)
-elementos=[random.randint(1, 10) for _ in range(num_elementos)]
-print("Lista de elementos:", elementos)
-soma=sum(elementos)
-print("Soma dos elementos:", soma)
-media=soma/num_elementos
-print("Média dos elementos:", media)
