5f33c202de44d2f81791c1887b456da3fe624b97
Ramon <127380359+RamonGodoi@users.noreply.github.com>
2024-06-21 22:01:35 -0300

    Delete 7.4 directory

--- 7.4/aula1_questao1.py
+++
@@ -1,7 +0,0 @@
-import os
-frase=input("Digite uma frase para salvar no arquivo 'frase.txt': ")
-diretorio_script= os.path.dirname(os.path.abspath(__file__))
-caminho_arquivo=os.path.join(diretorio_script, "frase.txt")
-with open(caminho_arquivo, 'w') as arquivo:
-    arquivo.write(frase)
-print("Frase salva no arquivo:", caminho_arquivo)
